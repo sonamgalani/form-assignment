b36ac51e347147343c5cca7661bf51af8168c2cb
Sonam Galani <arsapkale8@gmail.com>
2016-04-22 22:33:22 +0530

    Add all the files

---
+++ form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Student Form</h2>
+<p>* All fields are mandatory</p>
+<form action="display.php" method="post">
+Name:<br>
+<input type = "text" name="name" maxlength="40" required><small>(max 40 characters)</small><br>
+<br>
+MIS:<br>
+<input type = "text" name="mis" required><small>(9 digit number)</small><br>
+<br>
+Phone no.:<br>
+<input type="number" name="phone" maxlength="10" required>
+<br>
+<input type="submit" name="submit" value="Submit"> 
+</form>
+
+
+</body>
+</html>
+
